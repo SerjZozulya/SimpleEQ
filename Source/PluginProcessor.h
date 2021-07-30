56f1e1a1ad033cc8637059e3daad8399df5b7b73
Зозуля Сергей <SZozulya@lanit.ru>
2021-07-30 11:39:41 +0300

    Connected the LowCut Parameters to the Filter

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,11 +10,20 @@
 
 #include <JuceHeader.h>
 
+enum Slope
+{
+    SLOPE_12,
+    SLOPE_24,
+    SLOPE_36,
+    SLOPE_48
+};
+
 struct ChainSettings
 {
     float peakFreq{ 0 }, peakGainInDecibels{ 0 }, peakQuality{ 1.f };
     float lowCutFreq{ 0 }, highCutFreq{ 0 };
-    int lowCutSlope{ 0 }, highCutSlope{ 0 };
+
+    Slope lowCutSlope{ Slope::SLOPE_12 }, highCutSlope{  Slope::SLOPE_12 };
 };
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
