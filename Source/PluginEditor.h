73869949f022b7854c50f406fe61e4a67b335f88
Зозуля Сергей <SZozulya@lanit.ru>
2021-08-26 13:08:46 +0300

    Displaying basic param values

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -13,11 +13,11 @@
 
 struct LookAndFeel : juce::LookAndFeel_V4
 {
-    void drawRotarySlider(juce::Graphics&, int x, int y, int width, int height,
-        float sliderPosProportional,
-        float rotaryStartAngle,
-        float rotaryEndAngle,
-        juce::Slider&) override;
+    void drawRotarySlider (juce::Graphics&, int x, int y, int width, int height,
+                            float sliderPosProportional,
+                            float rotaryStartAngle,
+                            float rotaryEndAngle,
+                            juce::Slider&) override;
 };
 
 struct RotarySliderWithLabels : juce::Slider
